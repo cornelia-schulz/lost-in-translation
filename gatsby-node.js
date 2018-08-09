61d5d5c7547c765e645b809bedac5fa6723c66a0
Cornelia Schulz <cschulz@fraedom.com>
2018-08-10 11:11:01 +1200

    adding blog template

--- gatsby-node.js
+++ gatsby-node.js
@@ -35,5 +35,14 @@ exports.createPages = ({boundActionCreators, graphql}) => {
         if(result.errors) {
             return Promise.reject(result.errors)
         }
+
+        result.data.allMarkdownRemark.edges
+            .forEach(({node}) => {
+                createPage({
+                    path: node.frontmatter.path,
+                    component: blogPostTemplate,
+                    context: {} // additional data can be passed in via context
+                })
+            })
     })
 }
