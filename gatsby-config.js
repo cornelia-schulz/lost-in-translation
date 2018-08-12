dc7d2f4f61eddc08cbe66269e0a313c3dc05e3c2
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-08-12 12:12:02 +1200

    added rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,9 @@
 module.exports = {
   siteMetadata: {
     title: 'Lost in Translation',
-    author: 'Cornelia Schulz'
+    description: 'Localisation tips & tricks',
+    author: 'Cornelia Schulz',
+    siteUrl: 'https://www.cornelia-schulz.net'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -15,6 +17,58 @@ module.exports = {
         theme_color: '#663399',
         display: 'minimal-ui',
         icon: 'src/images/cs-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-feed',
+      options: {
+        query: `{
+          site {
+            siteMetadata {
+              title
+              description
+              siteUrl
+              site_url: siteUrl
+            }
+          }
+        }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                });
+              });
+            },
+            query: `
+            {
+              allMarkdownRemark(
+                limit: 1000,
+                sort: { order: DESC, fields: [frontmatter___date] },
+                filter: {frontmatter: { draft: { ne: true } }}
+              ) {
+                edges {
+                  node {
+                    excerpt
+                    html
+                    fields { slug }
+                    frontmatter {
+                      title
+                      date
+                    }
+                  }
+                }
+              }
+            }
+          `,
+            output: "/rss.xml",
+          },
+        ],
       },
     },
     'gatsby-plugin-catch-links',
@@ -40,23 +94,23 @@ module.exports = {
           'Roboto',
         ]
       },
-      },
-      `gatsby-plugin-sharp`,
-      {
+    },
+    `gatsby-plugin-sharp`,
+    {
       resolve: 'gatsby-transformer-remark',
-        options: {
-          plugins: [
-            {
-              resolve: `gatsby-remark-images`,
-              options: {
-                // It's important to specify the maxWidth (in pixels) of
-                // the content container as this plugin uses this as the
-                // base for generating different widths of each image.
-                maxWidth: 960,
-              },
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 960,
             },
-          ],
-        },
+          },
+        ],
       },
-    ]
-  }
+    },
+  ]
+}
