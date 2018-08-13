e84432117d060effeda523691f069984223bf2f7
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-08-13 19:52:58 +1200

    adding Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -111,5 +111,19 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-123814532-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
   ]
 }
